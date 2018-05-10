c2ba80be1bf188d9a9ef07d71611b062f4c412c8
suren-rathnayake <suren@maths.uq.edu.au>
2018-05-10 13:37:52 +1000

    deep..alg3.R fixed

--- R/deep.sem.alg.2.R
+++ R/deep.sem.alg.2.R
@@ -22,40 +22,40 @@ likelihood <- c(likelihood, temp)
 #####################################################
 while ((hh < it) & (ratio > eps )) {
   hh <- hh + 1
-###############################################################
-###################  first layer ##############################
-###############################################################
-l <- 1
-yy <- y
-z2 <- z2.one <- array(0, c(numobs, r[l+1], k[l], k[l+1]))
-zz2 <- array(0, c(numobs, r[l + 1], r[l + 1], k[l], k[l + 1]))
-z <- z.one <- array(0, c(numobs, r[l + 1], k[l]))
-zz <- array(0, c(numobs, r[l + 1], r[l + 1], k[l]))
+  ###############################################################
+  ###################  first layer ##############################
+  ###############################################################
+  l <- 1
+  yy <- y
+  z2 <- z2.one <- array(0, c(numobs, r[l+1], k[l], k[l+1]))
+  zz2 <- array(0, c(numobs, r[l + 1], r[l + 1], k[l], k[l + 1]))
+  z <- z.one <- array(0, c(numobs, r[l + 1], k[l]))
+  zz <- array(0, c(numobs, r[l + 1], r[l + 1], k[l]))
 
-for (p1 in 1 : k[l]) {
-  for (p2 in 1 : k[l + 1]) {
-    A <- ginv(H.list[[l + 1]][p2,, ] %*% t(H.list[[l + 1]][p2,, ]) +
-          psi.list[[l + 1]][p2,, ]) + t(H.list[[l]][p1,, ]) %*%
-          (psi.list.inv[[l]][p1,, ]) %*% (H.list[[l]][p1,, ])
+  for (p1 in 1 : k[l]) {
+    for (p2 in 1 : k[l + 1]) {
+      A <- ginv(H.list[[l + 1]][p2,, ] %*% t(H.list[[l + 1]][p2,, ]) +
+            psi.list[[l + 1]][p2,, ]) + t(H.list[[l]][p1,, ]) %*%
+            (psi.list.inv[[l]][p1,, ]) %*% (H.list[[l]][p1,, ])
 
-    b <- ginv(H.list[[l + 1]][p2,, ] %*% t(H.list[[l + 1]][p2,, ]) +
-          psi.list[[l + 1]][p2,, ]) %*%
-          matrix(mu.list[[l + 1]][, p2], r[l + 1], numobs) +
-          t(H.list[[l]][p1,, ]) %*% (psi.list.inv[[l]][p1,, ]) %*%
-          (t(yy) - matrix(mu.list[[l]][, p1], r[l], numobs))
+      b <- ginv(H.list[[l + 1]][p2,, ] %*% t(H.list[[l + 1]][p2,, ]) +
+            psi.list[[l + 1]][p2,, ]) %*%
+            matrix(mu.list[[l + 1]][, p2], r[l + 1], numobs) +
+            t(H.list[[l]][p1,, ]) %*% (psi.list.inv[[l]][p1,, ]) %*%
+            (t(yy) - matrix(mu.list[[l]][, p1], r[l], numobs))
 
-    chsi <- ginv(A)
-    if (!isSymmetric(chsi)) {
-      chsi <- makeSymm(chsi)
+      chsi <- ginv(A)
+      if (!isSymmetric(chsi)) {
+        chsi <- makeSymm(chsi)
+      }
+      roy <- chsi %*% b
+      roy.quadro <- array(apply(roy, 2, function(x) x %*% t(x)),
+                         c(r[l + 1], r[l + 1], numobs))
+      zz2[,,, p1, p2] <- aperm(array(chsi, c(r[l + 1], r[l + 1], numobs)) +
+                         roy.quadro, c(3, 1, 2))
+      z2.one[,, p1, p2] <- rmvnorm(numobs, rep(0, r[l + 1]), chsi) + t(roy)
+      z2[,,p1,p2] <- t(roy)
     }
-    roy <- chsi %*% b
-    roy.quadro <- array(apply(roy, 2, function(x) x %*% t(x)),
-                       c(r[l + 1], r[l + 1], numobs))
-    zz2[,,, p1, p2] <- aperm(array(chsi, c(r[l + 1], r[l + 1], numobs)) +
-                       roy.quadro, c(3, 1, 2))
-    z2.one[,, p1, p2] <- rmvnorm(numobs, rep(0, r[l + 1]), chsi) + t(roy)
-    z2[,,p1,p2] <- t(roy)
-  }
 }
 
 for (i in 1 : k[l + 1]) {
@@ -169,144 +169,3 @@ out <- list(H = H.list, w = w.list, mu = mu.list, psi = psi.list,
 return(out)
 }
 
-compute.lik <- function(y, numobs, k, mu.list, H.list, psi.list, w.list) {
-  value <- 1e-20
-  layers <- length(k)
-  p <- ncol(y)
-  py <- matrix(0,numobs)
-  tot.k <- prod(k)
-  py.s <- matrix(0,numobs,tot.k)
-  pys <- matrix(0,numobs,tot.k)
-  k.comb <- apply(t(k), 2, function(x) 1 : x)
-  if (is.list(k.comb)) {
-    k.comb <- expand.grid(k.comb)
-  }
-  if (is.matrix(k.comb)) {
-    k.comb <- expand.grid(split(t(k.comb), 1 : ncol(k.comb)))
-  }
-  if (prod(k) == 1) {
-    k.comb <- matrix(k.comb, nrow =1)
-  }
-
-  for (i in 1 : tot.k)  {
-    mu.tot <- mu.list[[1]][,k.comb[i, 1]]
-    var.tot <- psi.list[[1]][k.comb[i, 1],, ]
-    w.tot <- w.list[[1]][k.comb[i, 1]]
-    if (layers > 1) {
-      for (l in 2 : layers) {
-        tot.H <- diag(p)
-        for (m in 1 : (l - 1)) {
-          tot.H <- tot.H %*% H.list[[m]][k.comb[i, m],, ]
-        }
-        mu.tot <- mu.tot + tot.H %*% mu.list[[l]][, k.comb[i, l]]
-        var.tot <- var.tot + tot.H %*% (H.list[[l]][k.comb[i, l],, ] %*%
-                   t(H.list[[l]][k.comb[i, l],, ]) +
-                   psi.list[[l]][k.comb[i, l],, ]) %*% t(tot.H)
-        w.tot <- w.tot*w.list[[l]][k.comb[i,l]]
-      }
-    }
-
-    if (!is.positive.definite(var.tot)) {
-      var.tot <- make.positive.definite(var.tot)
-    }
-
-    py.s[,i] <- dmvnorm(y, c(mu.tot), as.matrix(var.tot), log = TRUE)
-
-    if (w.tot == 0) {
-      w.tot <- 10^(-320)
-    }
-    pys[, i] <- log(w.tot) + py.s[, i]
-  }
-
-  cc <- 705 - max(pys)
-  pys <- pys + cc
-  py <- rowSums(exp(pys))
-
-  ps.y <- exp(pys) / matrix(py, numobs, tot.k)
-  ps.y <- ifelse(is.na(ps.y), 1/k, ps.y)
-  py <- exp(-cc)*py
-
-  s <- matrix(0, numobs, layers)
-  ps.y.list <- NULL
-  for (l in 1 : layers)  {
-    psi.y <- matrix(0, numobs, k[l])
-    for (i in 1 : k[l]) {
-      index <- (k.comb[, l] == i)
-      psi.y[, i] <- rowSums(ps.y[, index, drop = FALSE])
-    }
-    s[, l] <- apply(psi.y, 1, which.max)
-    ps.y.list[[l]] <- psi.y
-  }
-
-return(list(py = py, py.s = py.s, ps.y = ps.y, k.comb = k.comb,
-   s = s, ps.y.list = ps.y.list))
-}
-
-compute.est <- function(k, p, qq, ps.y, y, Ez, Ezz, mu) {
-  value <- 1e-20
-  H <- array(0,c(k,p,qq))
-  psi <- psi.inv <- array(0,c(k,p,p))
-  numobs <- dim(y)[1]
-  w <- 0
-  for (i in 1:k) {
-   if (qq > 1) {
-    EEzz <- apply((Ezz[i,,, ] * (aperm(array(t(t(ps.y[, i])),
-               c(numobs, qq, qq)), c(2, 3, 1)))), 1, rowSums) / sum(ps.y[, i])
-   }
-
-   if (qq == 1) {
-     EEzz <- Ezz[i,,, ] %*% (ps.y[, i]) / sum(ps.y[, i])
-   }
-   H[i,, ] <- (t((y - t(matrix(mu[, i], p, numobs))) *
-               matrix(ps.y[,i], numobs, p)) %*%
-               (matrix(Ez[i,, ], nrow = numobs) *
-               matrix(ps.y[, i], numobs, qq))) %*% ginv(EEzz) / sum(ps.y[, i])
-
-   H[i,, ] <- ifelse(is.na(H[i,, ]), mean(H[i,, ], na.rm = TRUE), H[i,, ])
-
-   psi[i,,] <- (t((y - t(matrix(mu[, i], p, numobs))) *
-                matrix(ps.y[, i], numobs, p)) %*%
-                (matrix((y - t(matrix(mu[, i], p, numobs))) *
-                matrix(ps.y[, i], numobs, p), ncol = p)) -
-                t((y - t(matrix(mu[, i], p, numobs))) *
-                matrix(ps.y[, i], numobs, p)) %*%
-                (matrix(Ez[i,, ], nrow = numobs) *
-                matrix(ps.y[, i], numobs, qq)) %*% t(H[i,, ]))
-
-    psi[i,,] <- psi[i,,, drop = FALSE] / sum(ps.y[, i])
-    psi[i,,] <- ifelse((psi[i,,] == 0), value,psi[i,, ])
-    if (p > 1) {
-      psi[i,, ] <- diag(diag(psi[i,, ]))
-      psi.inv[i,, ] <- diag(1/diag(psi[i,, ]))
-    }
-    psi[i,, ]  <- ifelse(is.na(psi[i,, ]), 1, psi[i,, ])
-    psi.inv[i,,] <- ifelse(is.na(psi.inv[i,, ]), 1, psi.inv[i,, ])
-    mu[, i] <- colSums(matrix(ps.y[, i], numobs, p) *
-               (y - t(matrix(H[i,, ], ncol = qq) %*%
-               t(Ez[i,, ]))))/sum(ps.y[, i])
-    w[i] <- mean(ps.y[, i])
-  }
-  return(list(H = H,mu = mu, psi = psi, psi.inv = psi.inv, w = w))
-}
-
-makeSymm <- function(m) {
-  m[upper.tri(m)] <- t(m)[upper.tri(m)]
-  return(m)
-}
-
-ma <- function(x, n = 5) {
-  filter(x, rep(1/n, n), sides = 1)
-}
-
-entr <- function(z) {
-  numobs <- nrow(z)
-  numg <- ncol(z)
-  temp <- 0
-  z <- ifelse(z==0, z + 0.000000000000000000000001, z)
-  for (i in 1 : numg)  {
-    for (j in 1:numobs) {
-      temp <- temp + (z[j, i] *log(z[j, i]))
-    }
-  }
-  return(-temp)
-}
--- R/deepgmm.R
+++ R/deepgmm.R
@@ -24,7 +24,7 @@
 ###############################################################################
 
 deepgmm <- function(y, layers, k, r = rep(1, layers),
-                     it = 50, eps = 0.001, seed = 7, init = 'kmeans') {
+                     it = 50, eps = 0.001, init = 'kmeans') {
 
   if (any(tolower(init) == c('kmeans', 'k-means', 'k')))
     init <- 'kmeans'
@@ -37,146 +37,148 @@ deepgmm <- function(y, layers, k, r = rep(1, layers),
 
   if (class(y) == "data.frame") 
   	y <- as.matrix(y)
-  	 
+
   # check arguments
   tmp <- valid_args(Y = y, layers = layers, k = k, r = r, it = it, eps = eps, 
 						 init = init)
 
-  ptm <- proc.time()
-  a <- paste("Fit the model: seed=", seed, " k = (",paste(k, collapse=" "),
-  	   ") r = (",paste(r, collapse = " "), ")\n", sep = "")
-  cat(a)
-  set.seed(seed)
-  y <- scale(y)
+  # ptm <- proc.time()
+  # a <- paste("Fit the model: seed=", seed, " k = (",paste(k, collapse=" "),
+  # 	   ") r = (",paste(r, collapse = " "), ")\n", sep = "")
+  # cat(a)
+  # set.seed(seed)
+  # y <- scale(y)
 
   numobs <- nrow(y)
   p <- ncol(y)
   r <- c(p, r)
 
-#init
-w.list <- NULL
-H.list <- NULL
-psi.list <- NULL
-psi.list.inv <- NULL
-mu.list <- NULL
-z.list <- NULL
+  #init
+  w.list <- NULL
+  H.list <- NULL
+  psi.list <- NULL
+  psi.list.inv <- NULL
+  mu.list <- NULL
+  z.list <- NULL
 
-for (i in 1 : layers) {
+  for (i in 1 : layers) {
 
-  if (i == 1) {
-    data <- y
-  } else {
-    data <- z[, 1 : r[i], drop = FALSE]
-  }
+    if (i == 1) {
+      data <- y
+    } else {
+      data <- z[, 1 : r[i], drop = FALSE]
+    }
 
-  if (init == 'kmeans') {
-    if ( k[i] > 1) {
-      s <- kmeans(data, k[i], iter.max = 100, nstart = 30,
+    if (init == 'kmeans') {
+      if (k[i] > 1) {
+        s <- kmeans(data, k[i], iter.max = 100, nstart = 30,
               algorithm = "Hartigan-Wong")$cluster
-    } else {
-      s <- rep(1,numobs)
-    }
-  }
-
-  if (init == 'hclass') {
-    if (k[i] > 1) {
-      s <- hclass(hc(modelName = "VVV", data = y),k[i])
-    } else {
-      s <- rep(1, numobs)
-    }
-  }
-
-  if (init == 'random') {
-    if (k[i] > 1) {
-      s <- sample(1 : k[i], numobs, replace = TRUE)
-    } else {
-      s <- rep(1, numobs)
-    }
-  }
-
-  for  (j in 1 : k[i]) {
-    if ((table(s)[j]) < 2) {
-      s[sample(1 : numobs, 2, replace = FALSE)] <- j
-    }
-  }
-
-  psi <- psi.inv <- array(0, c(k[i], r[i], r[i]))
-  H <- array(0, c(k[i], r[i], r[i+1]))
-  mu <- matrix(0, r[i], k[i])
-  z <- NULL
-
-  for (j in 1 : k[i]) {
-    stima <- try(factanal(data[s == j, ], r[i+1], rotation="none",
-                 scores="Bartlett"), silent=TRUE)
-    if (is.character(stima)) {
-      psi[j,, ] <- 0.1 * diag(r[i])
-      psi.inv[j,, ] <- diag(r[i])
-      H[j,,] <- matrix(runif(r[i] * r[i+1]), r[i], r[i+1])
-      zt <- try(princomp(data[s == j, ])$scores[, 1 : r[i+1]], silent = TRUE)
-      if (!is.character(zt)) {
-        zt <- matrix(zt, ncol = r[i+1])
-      }
-      if (is.character(zt)) {
-        zt <- matrix(data[s == j, sample(1 : r[i+1])], ncol = r[i+1])
-        z <- rbind(z,zt)
+      } else {
+        s <- rep(1, numobs)
       }
     }
-    if (!is.character(stima)) {
-      psi[j,, ] <- diag(stima$uniq)
-      H[j,, ] <- stima$load
-      psi.inv[j,,] <- diag(1/stima$uniq)
-      z <- rbind(z, stima$scores)
+
+    if (init == 'hclass') {
+      if (k[i] > 1) {
+        s <- hclass(hc(modelName = "VVV", data = y), k[i])
+      } else {
+        s <- rep(1, numobs)
+      }
     }
-    mu[, j] <- colMeans(data[s == j,, drop = FALSE])
+
+    if (init == 'random') {
+      if (k[i] > 1) {
+        s <- sample(1 : k[i], numobs, replace = TRUE)
+      } else {
+        s <- rep(1, numobs)
+      }
+    }
+
+    for  (j in 1 : k[i]) {
+      if ((table(s)[j]) < 2) {
+        s[sample(1 : numobs, 2, replace = FALSE)] <- j
+      }
+    }
+
+    psi <- psi.inv <- array(0, c(k[i], r[i], r[i]))
+    H <- array(0, c(k[i], r[i], r[i+1]))
+    mu <- matrix(0, r[i], k[i])
+    z <- NULL
+
+    for (j in 1 : k[i]) {
+      stima <- try(factanal(data[s == j, ], r[i+1], rotation = "none",
+                   scores = "Bartlett"), silent = TRUE)
+
+      if (is.character(stima)) {
+        psi[j,, ] <- 0.1 * diag(r[i])
+        psi.inv[j,, ] <- diag(r[i])
+        H[j,,] <- matrix(runif(r[i] * r[i+1]), r[i], r[i+1])
+        zt <- try(princomp(data[s == j, ])$scores[, 1 : r[i+1]], silent = TRUE)
+        if (!is.character(zt)) {
+          zt <- matrix(zt, ncol = r[i+1])
+        }
+        if (is.character(zt)) {
+          zt <- matrix(data[s == j, sample(1 : r[i+1])], ncol = r[i+1])
+          z <- rbind(z,zt)
+        }
+      }
+
+      if (!is.character(stima)) {
+        psi[j,, ] <- diag(stima$uniq)
+        H[j,, ] <- stima$load
+        psi.inv[j,,] <- diag(1/stima$uniq)
+        z <- rbind(z, stima$scores)
+      }
+      mu[, j] <- colMeans(data[s == j,, drop = FALSE])
+    }
+
+    w <- matrix(table(s) / numobs)
+    w.list[i] <- list(w)
+    H.list[i] <- list(H)
+    mu.list[i] <- list(mu)
+    psi.list[i] <- list(psi)
+    psi.list.inv[i] <- list(psi.inv)
+    z.list[i] <- list(aperm(array(z[, 1 : r[i+1]], c(numobs, r[i+1], k[i])),
+                     c(3, 1, 2)))
   }
 
-  w <- matrix(table(s)/numobs)
-  w.list[i] <- list(w)
-  H.list[i] <- list(H)
-  mu.list[i] <- list(mu)
-  psi.list[i] <- list(psi)
-  psi.list.inv[i] <- list(psi.inv)
-  z.list[i] <- list(aperm(array(z[, 1 : r[i+1]], c(numobs, r[i+1], k[i])),
-                 c(3, 1, 2)))
- }
-
-##############################################################################
-if (layers == 1) {
-  out <- deep.sem.alg.1(y, numobs, p, r[2], k, H.list, psi.list,
+  ##############################################################################
+  if (layers == 1) {
+    out <- deep.sem.alg.1(y, numobs, p, r[2], k, H.list, psi.list,
+                        psi.list.inv, mu.list, w.list, z.list, it, eps) 
+  }
+  if (layers == 2) {
+    out <- deep.sem.alg.2(y, numobs, p, r, k, H.list, psi.list,
                         psi.list.inv, mu.list, w.list, z.list, it, eps)
-}
-if (layers == 2) {
-  out <- deep.sem.alg.2(y, numobs, p, r, k, H.list, psi.list,
+  }
+  if (layers == 3) {
+    out <- deep.sem.alg.3(y, numobs, p, r, k, H.list, psi.list,
                         psi.list.inv, mu.list, w.list, z.list, it, eps)
-}
-if (layers == 3) {
-  out <- deep.sem.alg.3(y, numobs, p, r, k, H.list, psi.list,
-                        psi.list.inv, mu.list, w.list, z.list, it, eps)
-}
+  }
 
-H <- out$H
-w <- out$w
-mu <- out$mu
-psi <- out$psi
-lik <- out$likelihood
-s <- out$s
-bic <- out$bic
-h <- out$h
-aic <- out$aic
-icl.bic <- out$icl.bic
-clc <- out$clc
+  H <- out$H
+  w <- out$w
+  mu <- out$mu
+  psi <- out$psi
+  lik <- out$likelihood
+  s <- out$s
+  bic <- out$bic
+  h <- out$h
+  aic <- out$aic
+  icl.bic <- out$icl.bic
+  clc <- out$clc
 
-output <- list(H = H.list, w = w.list, mu = mu.list, psi = psi.list, lik = lik,
-               bic = bic, aic = aic, clc = clc, s = s, icl.bic = icl.bic,
-               h = h, k = k, r = r, numobs = numobs)
-               #elapsed.time = proc.time() - ptm, seed = seed)
+  output <- list(H = H.list, w = w.list, mu = mu.list, psi = psi.list, lik = lik,
+                 bic = bic, aic = aic, clc = clc, s = s, icl.bic = icl.bic,
+                 h = h, k = k, r = r, numobs = numobs)
+                 #elapsed.time = proc.time() - ptm, seed = seed)
 
-class(output) <- "dgmm"
+  class(output) <- "dgmm"
 
-message("Estimation Details:")
-cat(paste("Log-Likelihood", round(lik[length(lik)], 2), "BIC:",
-      round(bic, 2), "AIC:", round(aic, 2), "\n"))
-cat("\n")
-invisible(output)
+  message("Estimation Details:")
+  cat(paste("Log-Likelihood", round(lik[length(lik)], 2), "BIC:",
+        round(bic, 2), "AIC:", round(aic, 2), "\n"))
+  cat("\n")
+  invisible(output)
 }
 ## s is the final classification
