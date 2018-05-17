9ad92a764d2c92cf6d737196b90dde7133e00c72
suren-rathnayake <suren@maths.uq.edu.au>
2018-05-17 16:19:48 +1000

    print and summary methods

--- R/print.R
+++ R/print.R
@@ -12,13 +12,27 @@ print.dgmm <- function(x, ...) {
     cat("\nCoefficients: \n")
     cat("pi_i : ", round(x$w[[lay]], 3), "\n")
 
-  
+
 	  for(j in 1 : x$k[lay]) {
 	    cat("mu_", j, ":\n")
 	    print(x$mu[[lay]][, j])
 	  }
 
-	  
+
 	}
 
+}
+
+summary.dgmm <- function(object, ...) {
+
+  cat("Call:\n")
+  print(object$call)
+  summ <- cbind(log_like = object$logL,
+                BIC = object$bic,
+                AIC = object$aic,
+                ICL.BIC = object$cls,
+                CIC = object$icl.bic
+              )
+  cat("\n")
+  print(summ)
 }
