9e5ba9a6a97116686f83f4c34c189e4596b4d9a1
suren-rathnayake <suren@maths.uq.edu.au>
2018-08-02 11:15:42 +1000

    just formatting of model.selection code

--- R/model.selection.R
+++ R/model.selection.R
@@ -11,7 +11,6 @@ pppp <- round(p / 4, 0)
 if (layers == 1) {
 
   r <- c(1 : pp)
-
   bic <- array(NA,c(seeds, pp))
   bic.best <- Inf
   aic <- array(NA,c(seeds, pp))
@@ -19,112 +18,112 @@ if (layers == 1) {
 
   for (i in 1 : seeds) for (rr in 1 : pp) {
 
-      set.seed(i)
-      out <- try(deepgmm(y, 1, k = g, rr, it = it, eps = eps, init = init,
+    set.seed(i)
+    out <- try(deepgmm(y, 1, k = g, rr, it = it, eps = eps, init = init,
                   method = method))
 
-      if (!is.character(out)) {
-        if (criterion=="BIC") if (out$bic < bic.best) {
+    if (!is.character(out)) {
+      if (criterion=="BIC") if (out$bic < bic.best) {
+        out.best <- out
+        bic.best <- out$bic
+      }
+      if (criterion=="AIC")
+        if (out$aic<aic.best) {
           out.best <- out
-          bic.best <- out$bic
+          aic.best <- out$aic
         }
-        if (criterion=="AIC")
-          if (out$aic<aic.best) {
-            out.best <- out
-            aic.best <- out$aic
-          }
 
         bic[i, rr] <- out$bic
         aic[i, rr] <- out$aic}
-    }
+  }
 
-    if (criterion == "BIC")
-      index <- which(bic == min(bic, na.rm = TRUE), arr.ind = TRUE)[1, ]
+  if (criterion == "BIC")
+    index <- which(bic == min(bic, na.rm = TRUE), arr.ind = TRUE)[1, ]
 
-    if (criterion == "AIC")
-      index <- which(aic == min(aic, na.rm = TRUE), arr.ind = TRUE)[1, ]
+  if (criterion == "AIC")
+    index <- which(aic == min(aic, na.rm = TRUE), arr.ind = TRUE)[1, ]
 
-    message("Best Fit: \n")
-    cat(paste("Seed=", index[1], " r=", index[2],
-              " BIC:", round(out.best$bic, 2), " AIC:",
-              round(out.best$aic, 2)))
+  message("Best Fit: \n")
+  cat(paste("Seed=", index[1], " r=", index[2],
+            " BIC:", round(out.best$bic, 2), " AIC:",
+            round(out.best$aic, 2)))
 }
 
 if (layers == 2) {
 
-    r <- as.matrix(expand.grid(1 : pp, 1 : ppp))
-    r <- r[(r[, 1]) > (r[, 2]), ]
-    k <- rbind(c(g, 1), c(g, 2), c(g, 3))
+  r <- as.matrix(expand.grid(1 : pp, 1 : ppp))
+  r <- r[(r[, 1]) > (r[, 2]), ]
+  k <- rbind(c(g, 1), c(g, 2), c(g, 3))
 
-    bic <- array(NA, c(seeds, nrow(k), nrow(r)))
-    bic.best <- Inf
-    aic <- array(NA, c(seeds, nrow(k), nrow(r)))
-    aic.best <- Inf
+  bic <- array(NA, c(seeds, nrow(k), nrow(r)))
+  bic.best <- Inf
+  aic <- array(NA, c(seeds, nrow(k), nrow(r)))
+  aic.best <- Inf
 
-    for (i in 1 : seeds)
-      for (kk in 1 : nrow(k))
-        for (rr in 1 : nrow(r)) {
-          set.seed(i)
-          out <- try(deepgmm(y, 2, k[kk, ], r[rr, ], it = it, eps = eps,
+  for (i in 1 : seeds)
+    for (kk in 1 : nrow(k))
+      for (rr in 1 : nrow(r)) {
+        set.seed(i)
+        out <- try(deepgmm(y, 2, k[kk, ], r[rr, ], it = it, eps = eps,
                       init = init, method = method))
-          if (!is.character(out)) {
-            if (criterion == "BIC")
-              if (out$bic < bic.best) {
+        if (!is.character(out)) {
+          if (criterion == "BIC")
+            if (out$bic < bic.best) {
+              out.best <- out
+              bic.best <- out$bic
+            }
+            if (criterion == "AIC")
+              if (out$aic < aic.best) {
                 out.best <- out
-                bic.best <- out$bic
+                aic.best <- out$aic
               }
-              if (criterion == "AIC")
-                if (out$aic < aic.best) {
-                  out.best <- out
-                  aic.best <- out$aic
-                }
 
-              bic[i, kk, rr] <- out$bic
-              aic[i, kk, rr] <- out$aic
-          }
+            bic[i, kk, rr] <- out$bic
+            aic[i, kk, rr] <- out$aic
         }
+      }
 
-    if (criterion == "BIC")
-      index <- which(bic == min(bic, na.rm = TRUE), arr.ind = TRUE)[1, ]
+if (criterion == "BIC")
+  index <- which(bic == min(bic, na.rm = TRUE), arr.ind = TRUE)[1, ]
 
-    if (criterion == "AIC")
-      index <- which(aic == min(aic, na.rm = TRUE), arr.ind = TRUE)[1, ]
+if (criterion == "AIC")
+  index <- which(aic == min(aic, na.rm = TRUE), arr.ind = TRUE)[1, ]
 
-    message("Best Fit: \n")
-    cat(paste("Seed=", index[1], " k=", paste(k[index[2],], collapse=" "),
-      " r=", paste(r[index[3], ], collapse = " "),
-      " BIC:", round(out.best$bic, 2), " AIC:", round(out.best$aic, 2)))
+message("Best Fit: \n")
+cat(paste("Seed=", index[1], " k=", paste(k[index[2],], collapse=" "),
+  " r=", paste(r[index[3], ], collapse = " "),
+  " BIC:", round(out.best$bic, 2), " AIC:", round(out.best$aic, 2)))
 }
 
 if (layers == 3) {
 
-    r <- as.matrix(expand.grid(1 : pp, 1 : ppp, 1 : ppp))
-    r <- r[((r[, 1]) > (r[, 2])) & ((r[, 2]) > (r[, 3])), ]
+  r <- as.matrix(expand.grid(1 : pp, 1 : ppp, 1 : ppp))
+  r <- r[((r[, 1]) > (r[, 2])) & ((r[, 2]) > (r[, 3])), ]
 
-    k <- rbind(c(g, 1, 1), c(g, 2, 1), c(g, 3, 1), c(g, 1, 2),
-               c(g, 2, 2), c(g, 3, 2), c(g, 1, 3), c(g, 2, 3),
-               c(g, 3, 3))
+  k <- rbind(c(g, 1, 1), c(g, 2, 1), c(g, 3, 1), c(g, 1, 2),
+             c(g, 2, 2), c(g, 3, 2), c(g, 1, 3), c(g, 2, 3),
+             c(g, 3, 3))
 
-    bic <- array(NA, c(seeds, nrow(k), nrow(r)))
-    bic.best <- Inf
-    aic <- array(NA, c(seeds, nrow(k), nrow(r)))
-    aic.best <- Inf
+  bic <- array(NA, c(seeds, nrow(k), nrow(r)))
+  bic.best <- Inf
+  aic <- array(NA, c(seeds, nrow(k), nrow(r)))
+  aic.best <- Inf
 
-    for (i in 1 : seeds)
-      for (kk in 1 : nrow(k))
-        for (rr in 1 : nrow(r)) {
+  for (i in 1 : seeds)
+    for (kk in 1 : nrow(k))
+      for (rr in 1 : nrow(r)) {
 
-          set.seed(i)
-          out <- try(deepgmm(y, 3, k[kk, ], r[rr, ], it = it, eps = eps,
+        set.seed(i)
+        out <- try(deepgmm(y, 3, k[kk, ], r[rr, ], it = it, eps = eps,
                       init = init, method = method))
-          if (!is.character(out)) {
-            if (criterion=="BIC")
-              if (out$bic < bic.best) {
-                out.best <- out
-                bic.best <- out$bic
-              }
+        if (!is.character(out)) {
+          if (criterion=="BIC")
+            if (out$bic < bic.best) {
+              out.best <- out
+              bic.best <- out$bic
+            }
 
-            if (criterion=="AIC")
+          if (criterion=="AIC")
             if (out$aic<aic.best) {
               out.best <- out
               aic.best <- out$aic
@@ -132,16 +131,16 @@ if (layers == 3) {
 
             bic[i,kk,rr] <- out$bic
             aic[i,kk,rr] <- out$aic
-          }
         }
+      }
 
-        if (criterion == "BIC")
-          index <- which(bic == min(bic, na.rm = TRUE), arr.ind = TRUE)[1, ]
+      if (criterion == "BIC")
+        index <- which(bic == min(bic, na.rm = TRUE), arr.ind = TRUE)[1, ]
 
-        if (criterion == "AIC")
-          index <- which(aic == min(aic, na.rm = TRUE), arr.ind = TRUE)[1, ]
+      if (criterion == "AIC")
+        index <- which(aic == min(aic, na.rm = TRUE), arr.ind = TRUE)[1, ]
 
-        message("Best Fit: \n")
+      message("Best Fit: \n")
         cat(paste("Seed=",index[1], " k=", paste(k[index[2], ], collapse=" "),
             " r=", paste(r[index[3], ], collapse=" "),
             " BIC:", round(out.best$bic, 2), " AIC:", round(out.best$aic, 2)))
